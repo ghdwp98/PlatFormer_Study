8385849c4512491d50733c940378acfb64c50c2b
JeongJaeHun <ghdwp98@naver.com>
2024-02-15 10:34:22 +0900

    docs : 몬스터 스크립터 생성

--- Assets/Scripts/Eagle.cs
+++ Assets/Scripts/Eagle.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Eagle : Monster
 {
+    private Transform playerTransform;
     void Start()
     {
-        
+        playerTransform = GameObject.FindWithTag("Player").transform; //캐싱 기법 공부
     }
 
     void Update()
